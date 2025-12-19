3a84a6aca13a6b1f193f4ba4b6e1a24a9e6c2a06
marceloHenrique <marcelo165171henrique@gmail.com>
2025-12-19 07:42:12 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,9 +1,43 @@
 import express from "express";
+import conectaNaDataBase from "./config/dbConect.js";
+import routes from "./routes/index.js";
+// import livro from "./models/livro.js";
+
+const conexao = await conectaNaDataBase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro)
+});
+
+conexao.once("open", () => {
+    console.log("conexão com o banco feito com sucesso")
+})
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200),send("curso de node.js");
+routes(app);
+
+
+
+app.get("/livros/:id", (req, res) =>{
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
 });
+
+app.post("/livros", (req, res) => {
+   livros.push(req.body);
+   res.status(201).send("livro cadastrado com sucesso");
+})
+
+app.put("/livros/:id", (req, res) => {
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
+
+app.delete("/livros/:id", (req, res) =>{
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("livro removido com sucesso");
+})
 
 export default app;
