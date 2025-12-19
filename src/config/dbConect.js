7568eada1c578fca1944d4ac4432c446430cfe04
marceloHenrique <marcelo165171henrique@gmail.com>
2025-12-19 07:44:28 -0300

    Delete src directory

--- src/config/dbConect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-async function conectaNaDataBase() {
-    mongoose.connect(process.env.DB_CONCECTION_STRING);
-    return mongoose.connection;
-};
-
-export default conectaNaDataBase;
